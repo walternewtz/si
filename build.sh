0cdc7e4d03dee17b71344ad48eaac55fce35398f
greengreen2212 <101193930+greengreen2212@users.noreply.github.com>
2022-07-01 10:24:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,9 +13,9 @@ export SKIP_ABI_CHECKS=true
 export SKIP_BOOTJAR_CHECKS=true
 export _JAVA_OPTIONS="-Xmx12g"
 
-make bacon -j30 &
-sleep 70m
-kill %1
+make bacon -j30 #&
+#sleep 70m
+#kill %1
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
